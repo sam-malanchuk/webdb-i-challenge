6183fedacb7814ee56c968cf415d23f3736f2b44
sam-malanchuk <malanchuksam@gmail.com>
2019-08-21 20:15:58 -0400

    new endpoint localhost/accounts returns the total accounts count

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 
-const db = require('./data/dbConfig.js');
+const knex = require('./data/dbConfig.js');
 
 const AccountsRouter = require('./accounts/accountsRouter.js');
 
@@ -12,6 +12,19 @@ server.use('/api/accounts', AccountsRouter);
 
 server.get('/', (req, res) => {
     res.send('<h3>Use the /api/accounts endpoint to make requests.</h3>');
+});
+
+server.get('/accounts', async (req, res) => {
+    try {
+        const count = await knex('accounts').count({ Accounts: 'id' });
+        if(count) {
+            res.json(count);
+        } else {
+            res.json({ message: "There are no accounts" });
+        }
+    } catch (error) {
+        res.status(500).json({ message: "Failed to count cities" });
+    }
 });
 
 module.exports = server;
